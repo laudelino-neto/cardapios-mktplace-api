156dbd40816f9f38858a6f9121271f17b7a8ea9b
laudelino-neto <laudelino_sc@hotmail.com>
2023-09-17 19:26:06 -0300

    Finalizada a integração para envio de e-mail

--- src/main/java/br/com/senai/cardapiosmktplaceapi/controller/CategoriaController.java
+++ src/main/java/br/com/senai/cardapiosmktplaceapi/controller/CategoriaController.java
@@ -39,7 +39,6 @@ public class CategoriaController {
 	@Qualifier("categoriaServiceProxy")
 	private CategoriaService service;	
 	
-	
 	@PostMapping
 	@Transactional
 	public ResponseEntity<?> inserir(
--- src/main/java/br/com/senai/cardapiosmktplaceapi/service/proxy/RestauranteServiceProxy.java
+++ src/main/java/br/com/senai/cardapiosmktplaceapi/service/proxy/RestauranteServiceProxy.java
@@ -1,11 +1,14 @@
 package br.com.senai.cardapiosmktplaceapi.service.proxy;
 
+import org.apache.camel.ProducerTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import br.com.senai.cardapiosmktplaceapi.dto.Notificacao;
 import br.com.senai.cardapiosmktplaceapi.entity.Categoria;
 import br.com.senai.cardapiosmktplaceapi.entity.Restaurante;
 import br.com.senai.cardapiosmktplaceapi.entity.enums.Status;
@@ -17,10 +20,33 @@ public class RestauranteServiceProxy implements RestauranteService {
 	@Autowired
 	@Qualifier("restauranteServiceImpl")
 	private RestauranteService service;
+	
+	@Value("${email.endereco-admin}")
+	private String enderecoDeEmail;
+
+	@Autowired
+	private ProducerTemplate toEmail;
 
 	@Override
 	public Restaurante salvar(Restaurante restaurante) {
-		return service.salvar(restaurante);
+		Restaurante restauranteSalvo = service.salvar(restaurante);
+		Notificacao notificacao = gerarNotificacaoParaNovo(restauranteSalvo);
+		this.toEmail.sendBody("direct:enviarEmail", notificacao);
+		return restauranteSalvo;
+	}
+	
+	private Notificacao gerarNotificacaoParaNovo(Restaurante restaurante) {
+		Notificacao notificacao = new Notificacao();
+		notificacao.setDestinatario(enderecoDeEmail);
+		notificacao.setTitulo("Novo Restaurante Criado");
+		StringBuilder texto = new StringBuilder();
+		texto.append("<p>Olá,</p>");
+		texto.append("<p>O restaurante <b>").append(restaurante.getNome() + "</b> foi criado no sistema.</p>");
+		texto.append("<p>Esse e-mail é automático. Não deve ser respondido.</p>");
+		texto.append("<p>Atenciosamente,</p>");
+		texto.append("<p><b>Mktplace SENAI</b></p>");
+		notificacao.setMensagem(texto.toString());
+		return notificacao;
 	}
 
 	@Override
